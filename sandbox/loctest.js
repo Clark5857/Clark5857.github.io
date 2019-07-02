ac818347c8440efb0305ccb225dc62fa4b42ddef
James Clark <jamesclark@205-185-96-169.directcom.com>
2019-07-01 21:13:59 -0600

    update all

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -2,6 +2,9 @@
 
 'use strict';
 
+// Call the function to get our location
+getGeoLocation();
+
  // Set global variable for custom header required by NWS API
  var idHeader = {
     headers: {
@@ -9,18 +12,15 @@
     }
   };
 
-var storage = window.localstorage; 
+var storage = window.localStorage; 
 
 // Gets longitude and latitude of current location
 function getGeoLocation() {
 
-} // end getGeoLocation
 
 const status = document.getElementById('status');
  status.innerHTML = 'Getting Location...';
 
- // Call the function to get our location
-getGeoLocation();
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -29,6 +29,11 @@ if (navigator.geolocation) {
   
      // Combine the values
      const locale = lat + "," + long;
+
+     // Call getLocation function, send locale
+  getLocation(locale);
+
+
      console.log(`Lat and Long are: ${locale}.`);
   
   
@@ -38,9 +43,94 @@ if (navigator.geolocation) {
    } // end else
   } //end getGeoLocation
 
-  console.log(`Lat and Long are: ${locale}.`);
+  // Gets location information from the NWS API
+function getLocation(locale) {
+  const URL = "https://api.weather.gov/points/" + locale; 
+  // NWS User-Agent header (built above) will be the second parameter 
+  fetch(URL, idHeader) 
+  .then(function(response){
+    if(response.ok){ 
+     return response.json(); 
+    } 
+    throw new ERROR('Response not OK.');
+  })
+  .then(function (data) { 
+    // Let's see what we got back
+    console.log('Json object from getLocation function:'); 
+    console.log(data);
+    // Store data to localstorage 
+    storage.setItem("locName", data.properties.relativeLocation.properties.city); 
+    storage.setItem("locState", data.properties.relativeLocation.properties.state); 
+ 
+    // Next, get the weather station ID before requesting current conditions 
+    // URL for station list is in the data object 
+    let stationsURL = data.properties.observationStations; 
+    // Call the function to get the list of weather stations
+    getStationId(stationsURL); 
+   }) 
+  .catch(error => console.log('There was a getLocation error: ', error)) 
+ } // end getLocation function
 
-  // Call getLocation function, send locale
-  getLocation(locale);
+// Gets weather station list and the nearest weather station ID from the NWS API
+function getStationId(stationsURL) { 
+  // NWS User-Agent header (built above) will be the second parameter 
+  fetch(stationsURL, idHeader) 
+  .then(function(response){
+    if(response.ok){ 
+     return response.json(); 
+    } 
+    throw new ERROR('Response not OK.');
+  })
+  .then(function (data) { 
+    // Let's see what we got back
+    console.log('From getStationId function:'); 
+    console.log(data);
+  
+    // Store station ID and elevation (in meters - will need to be converted to feet) 
+    let stationId = data.features[0].properties.stationIdentifier; 
+    let stationElevation = data.features[0].properties.elevation.value; 
+    console.log('Station and Elevation are: ' + stationId, stationElevation); 
+ 
+    // Store data to localstorage 
+    storage.setItem("stationId", stationId); 
+    storage.setItem("stationElevation", stationElevation); 
+ 
+    // Request the Current Weather for this station 
+    getWeather(stationId);
+   }) 
+  .catch(error => console.log('There was a getStationId error: ', error)) 
+ } // end getStationId function
+  
+ // Gets current weather information for a specific weather station from the NWS API
+function getWeather(stationId) { 
+  // This is the URL for current observation data 
+  const URL = 'https://api.weather.gov/stations/' + stationId + '/observations/latest';
+  // NWS User-Agent header (built above) will be the second parameter 
+  fetch(URL, idHeader) 
+  .then(function(response){
+    if(response.ok){ 
+     return response.json(); 
+    } 
+    throw new ERROR('Response not OK.');
+  })
+  .then(function (data) { 
+    // Let's see what we got back
+    console.log('From getWeather function:'); 
+    console.log(data);
+  
+    // Store weather information to localStorage 
+ storage.setItem("windChill", data.properties.windChill.value);
+ storage.setItem("windGust", data.properties.windGust.value);
+ storage.setItem("windSpeed", data.properties.windSpeed.value);
+ storage.setItem("temperature", data.properties.temperature.value);
+ storage.setItem("elevation", data.properties.elevation.value);
+ storage.setItem("highTemp", data.properties.maxTemperatureLast24Hours.value);
+ storage.setItem("lowTemp", data.properties.minTemperatureLast24Hours.value);
+ storage.setItem("percipitation", data.properties.percipitationLastHour.value);
+    // Build the page for viewing 
+    // viewPage()
+   }) 
+  .catch(error => console.log('There was a getWeather error: ', error)) 
+ } // end getWeather function
 
  
